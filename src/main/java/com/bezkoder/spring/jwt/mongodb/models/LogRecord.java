f18fffed9e7686889f7d6fb7cccf81623bef84c0
rusirumaduranga <50967832+rusirumaduranga@users.noreply.github.com>
2021-03-23 10:23:06 +0530

    Update LogRecord.java

---
+++ src/main/java/com/bezkoder/spring/jwt/mongodb/models/LogRecord.java
@@ -0,0 +1,83 @@
+package com.bezkoder.spring.jwt.mongodb.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "Log")
+public class LogRecord {
+
+    @Id
+    private String _id;
+    private double no;
+    private double time;
+    private String source;
+    private String destination;
+    private String protocol;
+    private double length;
+    private String info;
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public double getNo() {
+        return no;
+    }
+
+    public void setNo(double no) {
+        this.no = no;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
