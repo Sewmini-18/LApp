26b6a44f05d5b28cdf497b9f3cd9bb8e38c93c33
JMJ Riyana <jeemariyana@gmail.com>
2021-03-27 10:55:18 +0530

    FTP file retrieve

--- src/main/java/com/bezkoder/spring/jwt/mongodb/SpringBootSecurityJwtMongodbApplication.java
+++ src/main/java/com/bezkoder/spring/jwt/mongodb/SpringBootSecurityJwtMongodbApplication.java
@@ -1,7 +1,12 @@
 package com.bezkoder.spring.jwt.mongodb;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.File;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootSecurityJwtMongodbApplication {
@@ -10,4 +15,15 @@ public class SpringBootSecurityJwtMongodbApplication {
 		SpringApplication.run(SpringBootSecurityJwtMongodbApplication.class, args);
 	}
 
+	@Bean
+	public ApplicationRunner runner(FTPConfiguration.Gate gate, FTPConfiguration.GateFile gateFile) {
+		return args -> {
+			List list = gate.list(".");
+			System.out.println("Result:" + list);
+			List<File> files = gateFile.mget(".");
+			for (File file : files) {
+				System.out.println(file.getAbsolutePath());
+			}
+		};
+	}
 }
