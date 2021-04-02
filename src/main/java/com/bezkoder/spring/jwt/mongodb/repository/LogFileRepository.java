abb0e8dec6e978161330d3ad7b8743d49a995fba
JMJ Riyana <jeemariyana@gmail.com>
2021-04-02 17:10:14 +0530

    update charts

--- src/main/java/com/bezkoder/spring/jwt/mongodb/repository/LogFileRepository.java
+++ src/main/java/com/bezkoder/spring/jwt/mongodb/repository/LogFileRepository.java
@@ -2,10 +2,6 @@ package com.bezkoder.spring.jwt.mongodb.repository;
 
 import com.bezkoder.spring.jwt.mongodb.models.LogFile;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
-import java.util.List;
-
 public interface LogFileRepository extends MongoRepository<LogFile, String> {
 
 }
--- src/main/react-app/src/components/view/folder.component.js
+++ src/main/react-app/src/components/view/folder.component.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import axios from "axios";
-import { MDBDataTable, MDBNavLink } from "mdbreact";
-import { Link } from "react-router-dom";
+import {MDBDataTable, MDBNavLink} from "mdbreact";
+import {Link} from "react-router-dom";
 import HashLoader from "react-spinners/HashLoader";
 import CsvDownload from "react-json-to-csv";
 
 
-
 class Folder extends Component {
-    state = { folders: [], loading: false };
+    state = {folders: [], loading: false};
+
     async componentDidMount() {
         await axios
             .get("http://localhost:8080/api/auth/file")
@@ -18,28 +18,34 @@ class Folder extends Component {
                     folders: res.data,
                 });
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 console.log(error);
             });
         this.setState({
             loading: true
         })
     }
+
     render() {
         const data = {
             columns: [{
-                    label: "File Name",
-                    field: "fileName",
-                    sort: "asc",
-                },
+                label: "File Name",
+                field: "fileName",
+                sort: "asc",
+            },
                 {
                     label: "Date",
                     field: "date",
                     sort: "asc",
                 },
                 {
-                    label: "Action",
-                    field: "action",
+                    label: "Document",
+                    field: "action1",
+                    sort: "asc",
+                },
+                {
+                    label: "Chart",
+                    field: "action2",
                     sort: "asc",
                 },
             ],
@@ -47,71 +53,68 @@ class Folder extends Component {
                 ...this.state.folders.map((data, i) => ({
                     fileName: data.fileName,
                     date: data.date,
-                    action: ( <
-                        Link to = {
-                            { pathname: `/home/view/${data._id}` } } >
-                        <
-                        u style = {
-                            { color: 'blue' } } > view < /u> <
-                        /Link>
+                    action1: (
+                        <Link to={
+                            {pathname: `/home/view/${data._id}`}}>
+                            <u style={
+                                {color: 'blue'}}> open document < /u>
+                        </Link>
+                    ),
+                    action2: (
+                        <Link to={
+                            {pathname: `/home/chartcomponent/${data._id}`}}>
+                            <u style={
+                                {color: 'blue'}}> view chart < /u>
+                        </Link>
                     ),
                 })),
             ],
         };
-        return ( <
-            div >
-            <
-            div className = "container" >
-            <
-            div classname = "row g-3" >
-            <
-            div classname = "col" >
-            <
-            h2 className = "text-center my-5 text-weight-3 text-dark" >
-            Data Folders <
-            /h2> <
-            /div> <
-            /div> <
-            /div> <
-            div className = "container p-3" > {
-                this.state.loading ? ( <
-                    div >
-                    <
-                    MDBDataTable responsive striped bordered hover data = { data }
-                    /> <
-                    CsvDownload filename = "data.csv"
-                    style = {
-                        {
-                            display: "inline-block",
-                            cursor: "pointer",
-                            color: "#ffffff",
-                            fontSize: "15px",
-                            fontWeight: "bold",
-                            padding: "3px 6px",
-                        }
-                    }
-                    data = { this.state.folders } >
-                    <
-                    button type = "button"
-                    className = "btn btn-dark" >
-                    Download Data < i className = "fas fa-download p-2" > < /i> <
-                    /button> <
-                    /CsvDownload> <
-                    /div>
-                ) : ( <
-                    div className = "text-center"
-                    style = {
-                        { marginTop: "20%" } } >
-                    <
-                    HashLoader color = { "#292b2c" }
-                    loading = { true }
-                    size = { 150 }
-                    /> <
-                    /div>
-                )
-            } <
-            /div> <
-            /div>
+        return (
+            <div>
+                <div className="container">
+                    <div classname="row g-3">
+                        <div classname="col">
+                            <h2 className="text-center my-5 text-weight-3 text-dark">
+                                Data Folders
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="container p-3"> {
+                    this.state.loading ? (<
+                            div>
+                            <MDBDataTable responsive striped bordered hover data={data}/>
+                            <CsvDownload filename="data.csv"
+                                         style={
+                                             {
+                                                 display: "inline-block",
+                                                 cursor: "pointer",
+                                                 color: "#ffffff",
+                                                 fontSize: "15px",
+                                                 fontWeight: "bold",
+                                                 padding: "3px 6px",
+                                             }
+                                         }
+                                         data={this.state.folders}>
+                                <button type="button"
+                                        className="btn btn-dark">
+                                    Download Data <i className="fas fa-download p-2"> </i>
+                                </button>
+                            </CsvDownload>
+                        </div>
+                    ) : (<div className="text-center"
+                              style={
+                                  {marginTop: "20%"}}>
+                            <HashLoader color={"#292b2c"}
+                                        loading={true}
+                                        size={150}
+                            />
+                        </div>
+                    )
+                }
+                </div>
+            </div>
         );
     }
 }
