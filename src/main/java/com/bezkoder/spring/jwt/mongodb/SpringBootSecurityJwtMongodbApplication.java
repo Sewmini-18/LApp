911e623053ade3cbab3553b355ef4856537cd3e6
184072c <nkdamitha@gmail.com>
2021-04-18 23:29:47 +0530

    ftp tempary removed

--- src/main/java/com/bezkoder/spring/jwt/mongodb/SpringBootSecurityJwtMongodbApplication.java
+++ src/main/java/com/bezkoder/spring/jwt/mongodb/SpringBootSecurityJwtMongodbApplication.java
@@ -1,7 +1,5 @@
 package com.bezkoder.spring.jwt.mongodb;
 
-import com.bezkoder.spring.jwt.mongodb.LogImport.FTPConfiguration;
-import org.json.simple.JSONArray;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.bind.annotation.*;
 import com.bezkoder.spring.jwt.mongodb.LogImport.LogImport;
-
-import java.io.File;
-import java.util.*;
 
 @SpringBootApplication
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -31,13 +26,15 @@ public class SpringBootSecurityJwtMongodbApplication {
     }
 
     @Bean
-    public ApplicationRunner runner(FTPConfiguration.GateFile gateFile) {
+    public ApplicationRunner runner(
+            //FTPConfiguration.GateFile gateFile
+            ) {
         return args -> {
-            List<File> files = gateFile.mget(".");
-            for (File file : files) {
-                JSONArray arr = new JSONArray();
-                logimport.run(file, arr);
-            }
+//            List<File> files = gateFile.mget(".");
+//            for (File file : files) {
+//                JSONArray arr = new JSONArray();
+//                logimport.run(file, arr);
+//            }
         };
     }
 
